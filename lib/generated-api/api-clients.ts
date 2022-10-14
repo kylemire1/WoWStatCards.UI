49123ab7b0c0597818497dacfd8b749204606e37
Kyle Lemire <hello@klemire.com>
2022-10-13 22:15:32 -0500

    confirm cors error which means credentials are hitting the api

--- lib/generated-api/api-clients.ts
+++ lib/generated-api/api-clients.ts
@@ -1,9 +1,19 @@
-import { API_BASE_URL } from '../constants'
 import { AccountClient, CharacterStatsClient, StatCardsClient } from './StatCardApi'
 import fetch from 'isomorphic-fetch'
+import { RequestInit } from 'next/dist/server/web/spec-extension/request'
 
-export const characterStatsClient = new CharacterStatsClient(API_BASE_URL, {
-  fetch,
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
+const ssrSafeFetch = (url: RequestInfo, init?: RequestInit | undefined) => {
+  return fetch(url, { ...init, credentials: 'include' })
+}
+
+export const characterStatsClient = new CharacterStatsClient(API_URL, {
+  fetch: ssrSafeFetch,
 })
-export const statCardClient = new StatCardsClient(API_BASE_URL, { fetch })
-export const accountClient = new AccountClient(API_BASE_URL, { fetch })
+export const statCardClient = new StatCardsClient(API_URL, {
+  fetch: ssrSafeFetch,
+})
+export const accountClient = new AccountClient(API_URL, {
+  fetch: ssrSafeFetch,
+})
